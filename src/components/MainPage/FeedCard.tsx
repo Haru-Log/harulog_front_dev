2f52d3abecef3a45f6f1c996b7433b957225ff5f
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2024-01-17 20:45:16 +0900

    Fix: color code

--- src/components/MainPage/FeedCard.tsx
+++ src/components/MainPage/FeedCard.tsx
@@ -9,7 +9,7 @@ const FeedCard:React.FC<{img: any; name: string; likes: number; comment: number}
         <img src={img} alt="피드 이미지" className="rounded-xl  h-60 object-cover w-60"/>
       </div>
       <div className="flex flex-row w-[60%] justify-between text-xs h-6">
-        <div className="bg-main-color text-white px-3 py-1 rounded-full h-fit w-fit text-center">
+        <div className="bg-[#92C7CF] text-white px-3 py-1 rounded-full h-fit w-fit text-center">
           운동
         </div>
         <div className="flex flex-row h-full w-fit align-middle">
