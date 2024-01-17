518055e8f0035162e1f0409b573750fa2a4ac018
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2024-01-17 17:43:15 +0900

    modal temp

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { createContext, useState } from 'react'
 import Header from './Header/Header'
 import { Outlet } from 'react-router'
 import Footer from './Footer/Footer'
 
+type SetModalState = {
+  setLoginModal: any;
+  setRegisterModal: any;
+}
+
+type ModalState = {
+  loginModal: boolean;
+  registerModal: boolean;
+}
+
+export const SetModalContext = createContext<SetModalState | null>(null);
+export const ModalContext = createContext<ModalState | null>(null);
+
 const Layout = () => {
+
+  const [loginModal, setLoginModal] = useState(false);
+  const [registerModal, setRegisterModal] = useState(false);
+
   return (
     <div>
-      <Header />
+      <SetModalContext.Provider value={{ setLoginModal, setRegisterModal }}>
+        <Header setLoginModal={setLoginModal} setRegisterModal={setRegisterModal} />
+      </SetModalContext.Provider>
       <Outlet />
       <Footer />
     </div>
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Slogan from "../components/MainPage/Slogan"
 import ChallengeCard from "../components/MainPage/ChallengeCard"
 
@@ -18,7 +18,6 @@ const MainPage = () => {
     { name: '근력 운동 (주 4회)', img: achievement }
   ]
 
-  
   const dummy_feeds = [
     { name: '뼈맞음', img: nodap, likes: 3, comment: 5 },
     { name: '오운완', img: nodap, likes: 3, comment: 5 },
@@ -28,9 +27,9 @@ const MainPage = () => {
 
 
   return (
-    <div className="flex flex-col w-full h-full pl-5 pr-6">
+    <div className="flex flex-col w-full h-full pl-5 pr-6 items-center">
       <Slogan />
-      <div className="w-full flex flex-col mb-10">
+      <div className="w-[80%] flex flex-col mb-10">
         <div className="flex flex-row justify-between items-baseline w-full mb-5">
           <div className="text-xl">진행중인 챌린지</div>
           <div className="text-sm text-main-color">모든 챌린지</div>
@@ -41,15 +40,15 @@ const MainPage = () => {
           )}
         </div>
       </div>
-      <div className="w-full flex flex-col">
+      <div className="w-[80%] flex flex-col">
         <div className="flex flex-row justify-between items-baseline w-full mb-5">
           <div className="text-xl">진행중인 챌린지</div>
           <div className="text-sm text-main-color">가입하기</div>
         </div>
         <div className="flex flex-row justify-between">
           {
-            dummy_feeds.map((feed, idx) => 
-            <FeedCard key={idx} img={feed.img} name={feed.name} likes={feed.likes} comment={feed.comment} />
+            dummy_feeds.map((feed, idx) =>
+              <FeedCard key={idx} img={feed.img} name={feed.name} likes={feed.likes} comment={feed.comment} />
             )
           }
         </div>
