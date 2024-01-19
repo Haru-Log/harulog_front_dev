9dd474bb92a089fe81eaf26fbba8315a0073377a
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2024-01-19 11:20:43 +0900

    Chore: Category Type

--- src/types/Category.type.ts
+++ src/types/Category.type.ts
@@ -3,9 +3,16 @@ export interface Category {
   category: string;
 }
 
-const dummy_categories: Category[] = [
+export const dummy_categories: Category[] = [
   { category_id: 1, category: '운동' },
   { category_id: 2, category: '독서' },
   { category_id: 3, category: '공부' },
   { category_id: 4, category: '기상' },
+]
+
+export const category_themes: string[] = [
+  'bg-category_1',
+  'bg-category_2',
+  'bg-category_3',
+  'bg-category_4',
 ]
