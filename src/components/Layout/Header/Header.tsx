0ec6b7b8a63c6f6828d06b84e792e9bcf143b798
childevnote <daniel011125@gmail.com>
2024-01-20 03:33:24 +0900

    Design: some fix

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -8,10 +8,10 @@ const Header = () => {
   const [isHeaderVisible, setIsHeaderVisible] = useState(true);
 
   useEffect(() => {
-    let prevScrollPos = window.pageYOffset;
+    let prevScrollPos = document.documentElement.scrollTop;
 
     const handleScroll = () => {
-      const currentScrollPos = window.pageYOffset;
+      const currentScrollPos = document.documentElement.scrollTop;
       setIsHeaderVisible(currentScrollPos <= 0 || prevScrollPos > currentScrollPos);
       prevScrollPos = currentScrollPos;
     };
@@ -24,7 +24,7 @@ const Header = () => {
   }, []);
 
   return (
-    <div className={`flex w-full h-12 px-3 items-center justify-between bg-white border-b border-gray-400 z-50 ${isHeaderVisible ? 'fixed top-0' : ''}`}>
+    <div className={`flex w-full h-12 px-3 items-center justify-between bg-white border-b border-gray-400 z-50 transition-transform duration-900 ease-in-out ${isHeaderVisible ? 'fixed top-0 transform translateY(0)' : 'transform -translate-y-full'}`}>
       <TopNav />
       <Title />
       <div className='flex'>
