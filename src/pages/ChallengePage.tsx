f05c1e1cbfc2fc3c0fe1cc0bbcb040e75ce52df1
childevnote <daniel011125@gmail.com>
2024-01-20 02:59:31 +0900

    Design: challenge cards align center

--- src/pages/ChallengePage.tsx
+++ src/pages/ChallengePage.tsx
@@ -1,14 +1,16 @@
+import Cards from '../components/ChallengePage/Cards'
 import CreateButton from '../components/CreateButton'
 import FilterGroup from '../components/FilterGroup'
+import dummyChallengeData from '../types/ChallengeItem.dummy'
 
 const ChallengePage = () => {
-
   return (
-    <div className='flex'>
+    <div className='flex flex-col items-center'>
       <FilterGroup />
       <CreateButton />
+      <Cards cards={dummyChallengeData} />
     </div>
-  )
-}
+  );
+};
 
 export default ChallengePage
