b52c24cb671d1e95779b854cc011c19229af3a5c
childevnote <daniel011125@gmail.com>
2024-01-20 03:15:17 +0900

    Design: challenge cards

--- src/components/ChallengePage/Card.tsx
+++ src/components/ChallengePage/Card.tsx
@@ -18,15 +18,15 @@ const Card: React.FC<ChallengeItem> = ({ challenge_id, category_id, chatroom_id,
   }, [])
 
   return (
-    <div className="cursor-pointer mb-3 relative group transform transition-transform hover:scale-110" onClick={() => navigate(`/challenge/${challenge_id}`)}>
-      <div className="w-72 h-72 overflow-hidden rounded-xl">
+    <div className="cursor-pointer transform transition-transform hover:scale-110 drop-shadow-xl" onClick={() => navigate(`/challenge/${challenge_id}`)}>
+      <div className="w-72 h-72 rounded-xl">
         <img src={challenge_image} alt="챌린지 이미지" className="object-cover w-full h-full rounded-xl" />
       </div>
       <div className="absolute bottom-0 left-0 right-0 p-3 pt-7 bg-gradient-to-b from-transparent to-slate-800 rounded-b-xl">
-        <span className="text-white text-md font-bold">{challenge_title}</span>
+        <span className="text-white text-lg font-semibold">{challenge_title}</span>
         <div className="flex flex-col items-center absolute bottom-0 right-0 pb-3 pr-3">
           <Flame color="white" strokeWidth={4} />
-          <span className="text-white text-xs">{participants}</span>
+          <span className="text-white text-xs mt-1 font-semibold">{participants}</span>
         </div>
       </div>
     </div>
