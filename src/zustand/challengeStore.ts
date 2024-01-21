67808e3ac4f41d1f1a21daa77d1f641bf950da90
childevnote <daniel011125@gmail.com>
2024-01-22 02:27:42 +0900

    Docs: challenge store

---
+++ src/zustand/challengeStore.ts
@@ -0,0 +1,34 @@
+import { create } from 'zustand';
+import { devtools, persist } from 'zustand/middleware';
+import { ChallengeItem } from '../types/ChallengeItem.type';
+
+interface ChallengeState {
+  challenge: ChallengeItem;
+  setChallenge: (challenge: ChallengeItem) => void;
+}
+
+export const useChallengeStore = create<ChallengeState>()(
+  devtools(
+    persist(
+      (set) => ({
+        challenge: {
+          challenge_id: 0,
+          category_id: 0,
+          chatroom_id: 0,
+          challenge_title: '',
+          challenge_content: '',
+          challenge_goal: '',
+          challenge_image: '',
+          participants: 0,
+          submission: '',
+          start_date: new Date(),
+          end_date: new Date(),
+          created_at: new Date(),
+          updated_at: undefined,
+        },
+        setChallenge: (challenge: ChallengeItem) => set({ challenge }),
+      }),
+      { name: 'challengeStore' }
+    ),
+  ),
+);
