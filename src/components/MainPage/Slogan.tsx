21e0074ad43bed459c2b5a908716b82095b1d092
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2024-01-23 23:43:11 +0900

    Feat: MainPage shows dummy challenges

--- src/components/MainPage/Slogan.tsx
+++ src/components/MainPage/Slogan.tsx
@@ -1,28 +1,31 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import achievement from '../../assets/achievement-5597527_640.png'
+import { SetModalContext } from "../../App";
 
 const Slogan = () => {
+
+  const setRegisterModal = useContext(SetModalContext)?.setRegisterModal;
   return (
     <div className="flex flex-row w-full align-baseline max-h-64">
-        <div className="flex flex-col w-[60%] items-center mt-10">
-          <div className="flex flex-col">
-            <div className="text-3xl mb-5">
-              매일 1%의 성장기록
-            </div>
-            <div>
-              운동과 독서, 기상 인증 등 목표가 무엇이든 HaruLog에는 사람들이 각자의 성장을 공유합니다.
-              <br />
-              매일 반복되는 하루, 기록을 통해 성장하는 자신을 발견해보세요.
-            </div>
+      <div className="flex flex-col w-[60%] items-center mt-10">
+        <div className="flex flex-col">
+          <div className="text-3xl mb-5">
+            매일 1%의 성장기록
           </div>
-          <button className="bg-[#92C7CF] w-fit px-5 py-2 text-white rounded-xl mt-5">
-            가입하기
-          </button>
+          <div>
+            운동과 독서, 기상 인증 등 목표가 무엇이든 HaruLog에는 사람들이 각자의 성장을 공유합니다.
+            <br />
+            매일 반복되는 하루, 기록을 통해 성장하는 자신을 발견해보세요.
+          </div>
         </div>
-        <div className="w-[40%] flex">
-          <img src={achievement} alt="Achievement" className="w-full max-w-96 h-fit min-w-60 object-contain" />
-        </div>
+        <button className="bg-[#92C7CF] w-fit px-5 py-2 text-white rounded-xl mt-5" onClick={() => { setRegisterModal(true)} }>
+          가입하기
+        </button>
       </div>
+      <div className="w-[40%] flex">
+        <img src={achievement} alt="Achievement" className="w-full max-w-96 h-fit min-w-60 object-contain" />
+      </div>
+    </div>
   )
 }
 
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,26 +1,35 @@
 import Slogan from "../components/MainPage/Slogan"
-import ChallengeCard from "../components/MainPage/ChallengeCard"
 
 // dummy images
-import achievement from '../assets/achievement-5597527_640.png'
 import nodap from '../assets/20231010_084411.jpg'
 import FeedCard from "../components/MainPage/FeedCard"
+import { useContext, useEffect, useState } from "react"
+import { SetModalContext } from "../App"
+import dummyChallengeData from './../types/ChallengeItem.dummy';
+import { ChallengeItem } from "../types/ChallengeItem.type"
+import ChallengeCard from "../components/ChallengePage/ChallengeCard"
+
 
 const MainPage = () => {
-  const dummy_chals = [
-    { name: '한 달 영어완성 챌린지', img: achievement },
-    { name: '아침 조깅', img: achievement },
-    { name: '독서하기 (주 3회)', img: achievement },
-    { name: '근력 운동 (주 4회)', img: achievement }
-  ]
-
   const dummy_feeds = [
     { name: '뼈맞음', img: nodap, likes: 3, comment: 5 },
     { name: '오운완', img: nodap, likes: 3, comment: 5 },
     { name: '인상깊었던 구절', img: nodap, likes: 3, comment: 5 },
   ]
 
+  const [challenges, setChallenges] = useState<ChallengeItem[]>([]);
 
+  useEffect(() => {
+    const challengeItems = [...dummyChallengeData];
+    const showItems = []
+    while (showItems.length < 4) {
+      showItems.push(challengeItems.splice(Math.floor(Math.random() * challengeItems.length), 1)[0])
+    }
+    setChallenges(showItems)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dummyChallengeData])
+
+  const setRegisterModal = useContext(SetModalContext)?.setRegisterModal;
 
   return (
     <div className="flex flex-col w-full h-full pl-5 pr-6 pt-12 items-center">
@@ -31,15 +40,17 @@ const MainPage = () => {
           <div className="text-sm text-[#92C7CF]">모든 챌린지</div>
         </div>
         <div className="flex flex-row justify-between">
-          {dummy_chals.map((ch, idx) =>
-            <ChallengeCard key={idx} img={ch.img} name={ch.name} />
-          )}
+          {
+            challenges.map((item) =>
+              <ChallengeCard key={item.challenge_id} {...item} />
+            )
+          }
         </div>
       </div>
       <div className="w-[80%] flex flex-col">
         <div className="flex flex-row justify-between items-baseline w-full mb-5">
-          <div className="text-xl">진행중인 챌린지</div>
-          <div className="text-sm text-[#92C7CF]">가입하기</div>
+          <div className="text-xl">피드</div>
+          <div className="text-sm text-[#92C7CF] cursor-pointer" onClick={() => { setRegisterModal(true) }}>가입하기</div>
         </div>
         <div className="flex flex-row justify-between">
           {
