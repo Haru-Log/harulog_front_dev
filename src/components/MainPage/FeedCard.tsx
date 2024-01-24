341ac883be6726795ba1b141a8b06e944d2d3361
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2024-01-24 20:55:55 +0900

    Feat: Homepage slide

--- src/components/MainPage/FeedCard.tsx
+++ src/components/MainPage/FeedCard.tsx
@@ -1,29 +1,29 @@
 import React from 'react'
 import { Heart, MessageSquareMore } from "lucide-react";
 
-const FeedCard: React.FC<{ img: any; name: string; likes: number; comment: number }> = ({ img, name, likes, comment }) => {
+const FeedCard: React.FC<{ post_image: any; category_name: string; like: number; comment: number; content: string }> = ({ post_image, category_name, like, comment, content }) => {
+
   return (
-    <div className="w-60 object-scale-down flex flex-col items-start">
-      <div className="mb-3">
-        <img src={img} alt="피드 이미지" className="rounded-xl  h-60 object-cover w-60" />
+    <div className="cursor-pointer drop-shadow-xl mb-5">
+      <div className="w-72 h-72 rounded-xl">
+        <img src={post_image} alt="챌린지 이미지" className="object-cover w-full h-full rounded-xl" />
       </div>
-      <div className="flex flex-row w-[60%] justify-between text-xs h-6">
-        <div className="bg-[#92C7CF] text-white px-3 py-1 rounded-full h-fit w-fit text-center">
-          운동
+      <div className="flex flex-row w-full justify-start text-xs h-6 mt-2">
+        <div className={`text-white px-3 py-1 rounded-full h-fit w-fit text-center mr-5 bg-${category_name}`}>
+          {category_name}
         </div>
-        <div className="flex flex-row h-full w-fit align-middle">
+        <div className="flex flex-row h-fit mr-5 items-start">
           <Heart />
-          <div className="text-xl pb-2 w-fit h-full ml-1">{likes}</div>
+          <div className="text-xl pb-2 w-fit h-full ml-1">{like}</div>
         </div>
-        <div className="flex flex-row">
+        <div className="flex flex-row h-fit items-start">
           <MessageSquareMore />
-          <div className="text-xl pb-2 w-fit h-full ml-1">{comment}</div>
+          <div className="text-xl w-fit h-full ml-1">{comment}</div>
         </div>
       </div>
       <div>
-        {name}
+        {content}
       </div>
-
     </div>
   )
 }
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,22 +1,14 @@
 import Slogan from "../components/MainPage/Slogan"
-
-// dummy images
-import nodap from '../assets/20231010_084411.jpg'
-import FeedCard from "../components/MainPage/FeedCard"
 import { useContext, useEffect, useState } from "react"
 import { SetModalContext } from "../App"
 import dummyChallengeData from './../types/ChallengeItem.dummy';
 import { ChallengeItem } from "../types/ChallengeItem.type"
 import ChallengeCard from "../components/ChallengePage/ChallengeCard"
+import FeedSlide from "../components/MainPage/FeedSlide"
+import { dummy_sample } from "../types/FeedItem.type"
 
 
 const MainPage = () => {
-  const dummy_feeds = [
-    { name: '뼈맞음', img: nodap, likes: 3, comment: 5 },
-    { name: '오운완', img: nodap, likes: 3, comment: 5 },
-    { name: '인상깊었던 구절', img: nodap, likes: 3, comment: 5 },
-  ]
-
   const [challenges, setChallenges] = useState<ChallengeItem[]>([]);
 
   useEffect(() => {
@@ -34,7 +26,7 @@ const MainPage = () => {
   return (
     <div className="flex flex-col w-full h-full pl-5 pr-6 pt-12 items-center">
       <Slogan />
-      <div className="w-[80%] flex flex-col mb-10">
+      <div className="flex flex-col mb-10 w-full px-36">
         <div className="flex flex-row justify-between items-baseline w-full mb-5">
           <div className="text-xl">진행중인 챌린지</div>
           <div className="text-sm text-[#92C7CF]">모든 챌린지</div>
@@ -47,18 +39,14 @@ const MainPage = () => {
           }
         </div>
       </div>
-      <div className="w-[80%] flex flex-col">
+      <div className="w-full flex flex-col px-36">
         <div className="flex flex-row justify-between items-baseline w-full mb-5">
           <div className="text-xl">피드</div>
           <div className="text-sm text-[#92C7CF] cursor-pointer" onClick={() => { setRegisterModal(true) }}>가입하기</div>
         </div>
-        <div className="flex flex-row justify-between">
-          {
-            dummy_feeds.map((feed, idx) =>
-              <FeedCard key={idx} img={feed.img} name={feed.name} likes={feed.likes} comment={feed.comment} />
-            )
-          }
-        </div>
+
+        <FeedSlide data={dummy_sample} />
+
       </div>
     </div>
   )
