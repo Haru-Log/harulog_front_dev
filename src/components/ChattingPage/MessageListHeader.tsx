a3c5c94140ac916f67f8452cf2577f717541cf0b
childevnote <daniel011125@gmail.com>
2024-01-29 10:36:00 +0900

    Design: some fix

--- src/components/ChattingPage/MessageListHeader.tsx
+++ src/components/ChattingPage/MessageListHeader.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const MessageListHeader = () => {
   return (
-    <div className="text-lg font-bold leading-none bg-point pt-4 pb-2 px-4 flex justify-between items-start">
+    <div className="sticky top-0 z-10 text-lg font-bold leading-none bg-[#B8D8DD] pt-4 pb-2 px-4 flex justify-between items-start">
     <span>메세지</span>
     <div className='flex flex-col items-center justify-center'>
       <SquarePen className='cursor-pointer' size={15} />
