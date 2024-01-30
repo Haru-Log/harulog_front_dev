2c150fde2cfc417efe29f40fd86db1f64f77c0bc
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2024-01-30 22:32:49 +0900

    Fix: Render after img loaded

--- src/components/Feed/Cards.jsx
+++ src/components/Feed/Cards.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import MagicGrid from "react-magic-grid"
 import FeedCard from "./FeedCard"
 import { useFilterStore } from 'src/zustand/filterStore.ts';
@@ -7,14 +7,21 @@ const Cards = ({ data }) => {
 
   const { selectedValue } = useFilterStore();
   const filteredCards = selectedValue === '전체' ? data : data.filter(item => item.category_name === selectedValue);
+  const [isImgLoaded, setIsImgLoaded] = useState([])
+
+  useEffect(() => {
+    setIsImgLoaded(data.map(x => false))
+  }, [data])
 
   return (
     <div className="w-full h-full p-12 mt-8">
+      { 
+      isImgLoaded && isImgLoaded.reduce((prev, curr)=>prev && curr, true) &&
       <MagicGrid items={data.length} gutter={30} animate={true}>
-        {filteredCards.map(item => (
-            <FeedCard key={item.post_id} {...item} />
+        {filteredCards.map((item, idx) => (
+          <FeedCard key={item.post_id} {...item} idx={idx} setIsImgLoaded={setIsImgLoaded} isImgLoaded={isImgLoaded} />
         ))}
-      </MagicGrid>
+      </MagicGrid>}
     </div>
   )
 }
