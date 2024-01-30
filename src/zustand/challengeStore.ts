4e301a45f7ad4341385a871573fb414a91b50884
childevnote <daniel011125@gmail.com>
2024-01-30 22:39:55 +0900

    Remove: persist localstorage

--- src/zustand/challengeStore.ts
+++ src/zustand/challengeStore.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand';
-import { devtools, persist } from 'zustand/middleware';
+import { devtools } from 'zustand/middleware';
 import { ChallengeItem } from '../types/ChallengeItem.type';
 
 interface ChallengeState {
@@ -9,28 +9,26 @@ interface ChallengeState {
 
 export const useChallengeStore = create<ChallengeState>()(
   devtools(
-    persist(
-      (set) => ({
-        challenge: {
-          challenge_id: 0,
-          category_id: 0,
-          category_name: '',
-          chatroom_id: 0,
-          challenge_title: '',
-          challenge_content: '',
-          challenge_goal: 0,
-          challenge_image: '',
-          participants: 0,
-          submission: '',
-          start_date: new Date(),
-          end_date: new Date(),
-          created_at: new Date(),
-          updated_at: undefined,
-        },
-        setChallenge: (challenge: ChallengeItem) => set({ challenge }),
-        
-      }),
-      { name: 'challengeStore' }
-    ),
+    (set) => ({
+      challenge: {
+        challenge_id: 0,
+        category_id: 0,
+        category_name: '',
+        chatroom_id: 0,
+        challenge_title: '',
+        challenge_content: '',
+        challenge_goal: 0,
+        challenge_image: '',
+        participants: 0,
+        submission: '',
+        start_date: new Date(),
+        end_date: new Date(),
+        created_at: new Date(),
+        updated_at: undefined,
+      },
+      setChallenge: (challenge: ChallengeItem) => set({ challenge }),
+
+    }),
+    { name: 'challengeStore' }
   ),
 );
