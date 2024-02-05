e4853ec268b2a4c45fedc5badd3d599eb7f7383e
childevnote <daniel011125@gmail.com>
2024-02-05 16:17:15 +0900

    Design: some fix

--- src/components/ChallengeDetailPage/ChallengeInfo.tsx
+++ src/components/ChallengeDetailPage/ChallengeInfo.tsx
@@ -4,7 +4,7 @@ import ChallengeInfoRight from './ChallengeInfoRight';
 
 const ChallengeInfo = () => {
   return (
-    <div className='flex flex-col mx-10 mt-10 border-2 rounded-xl px-10 py-10'>
+    <div className='flex flex-col mt-10 border-2 rounded-xl px-10 py-10'>
       <ChallengeTopBadge />
       <div className='flex flex-row mt-4'>
         <ChallengeInfoLeft />
