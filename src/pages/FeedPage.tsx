9a987454aec9ec338c9b442da6ce4291f86e505b
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2024-02-05 18:21:37 +0900

    Fix: get feed items from express server and firebase storage

--- src/pages/FeedPage.tsx
+++ src/pages/FeedPage.tsx
@@ -1,13 +1,29 @@
 import Cards from "../components/Feed/Cards"
-import { dummy_sample } from "../types/FeedItem.type"
+import { FeedItem } from "../types/FeedItem.type"
 import CreateButton from '../components/CreateButton'
 import FilterGroup from '../components/FilterGroup'
+import { useEffect, useState } from "react"
+import axios from "../api/axios"
+
 
 const FeedPage = () => {
+
+  const [feedItems, setFeedItems] = useState<FeedItem[]>([])
+
+  useEffect(() => {
+
+    const fetchFeedItems = async () => {
+      const response = await axios.get('/feed')
+      setFeedItems(response.data.data);
+    }
+    fetchFeedItems()
+
+  }, [])
+
   return (
     <div className='flex flex-col font-ibm'>
       <FilterGroup />
-      <Cards data={dummy_sample} />
+      {feedItems.length > 0 && <Cards data={feedItems} />}
       <CreateButton />
     </div>
   )
