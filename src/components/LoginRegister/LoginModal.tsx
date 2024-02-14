87890b43bff43ebd21529774f724e63a56ba36ef
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2024-02-14 22:04:06 +0900

    Fix: login

--- src/components/LoginRegister/LoginModal.tsx
+++ src/components/LoginRegister/LoginModal.tsx
@@ -28,7 +28,7 @@ const LoginModal = () => {
     const response = await sendLoginRequest(userInfo)
     if (response.code === "COM-000") {
       setLoginModal(false);
-      window.location.reload()
+      // window.location.reload()
     } else {
       alert('로그인 실패')
     }
