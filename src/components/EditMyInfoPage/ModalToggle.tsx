c02909420169c18697d58eeb6920d1001960eadc
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2024-02-15 21:13:10 +0900

    Feat: Delete Account

--- src/components/EditMyInfoPage/ModalToggle.tsx
+++ src/components/EditMyInfoPage/ModalToggle.tsx
@@ -1,32 +1,20 @@
-import { Eye, EyeOff } from 'lucide-react';
-import React, { useState } from 'react';
+import React from 'react';
 // import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const ModalToggle: React.FC = () => {
-  const [passwordVisible, setPasswordVisible] = useState(false);
-
-  const togglePasswordVisibility = () => {
-    setPasswordVisible(!passwordVisible);
-  };
-
+const ModalToggle: React.FC<{ email: string; setEmail: React.Dispatch<React.SetStateAction<string>> }> = ({ email, setEmail }) => {
   return (
     <div className="modal">
       <div className="modal-content flex items-center">
         <input
-          type={passwordVisible ? 'text' : 'password'}
-          placeholder="계정을 삭제하려면 비밀번호를 입력하세요."
+          type='text'
+          placeholder="계정을 삭제하려면 이메일을 입력하세요."
           id="passwordInput"
           className="w-full text-xs p-2 border border-gray-100 rounded-md mr-2"
           style={{ outline: '1px solid #84B4BB' }}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
-        <span className="cursor-pointer" onClick={togglePasswordVisibility}>
-          {/* <FontAwesomeIcon
-            icon={passwordVisible ? faEye : faEyeSlash}
-            style={{ position: 'relative', right: '55px', color: 'grey' }}
-          /> */}
-          {passwordVisible ? <Eye className='relative right-14 text-gray-400' /> : <EyeOff className='relative right-14 text-gray-400' />}
-        </span>
       </div>
     </div>
   );
