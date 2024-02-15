03ca698e3388e544f513ed960175bac6e36ae415
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2024-02-15 16:07:29 +0900

    Feat: Edit Goal

--- src/api/grow/EditGoal.js
+++ src/api/grow/EditGoal.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const editGoal = async (updateGoalsList) => {
   try {
     const accessToken = localStorage.getItem('AccessToken');
-    const response = await axios.post(`${process.env.REACT_APP_BACKEND_DEPLOY}api/user-goal/update`,updateGoalsList,
+    const response = await axios.put(`${process.env.REACT_APP_BACKEND_DEPLOY}api/user-goal/update`,updateGoalsList,
     {
       headers: {
         Authorization: `Bearer ${accessToken}`
--- src/components/GrowPage/GetUpRow.tsx
+++ src/components/GrowPage/GetUpRow.tsx
@@ -1,8 +1,8 @@
 import { Input } from "../../ui/input";
 import { TableCell, TableRow } from "../../ui/table";
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const MyGoalRow: React.FC<{
+const GetUpRow: React.FC<{
   isEdit: boolean;
   category: string;
   userGoal: number;
@@ -13,28 +13,35 @@ const MyGoalRow: React.FC<{
   myGoal: any
 }> = ({ isEdit, category, userGoal, updatedAt, achievement, isLastRow, setMyGoal, myGoal }) => {
 
+  const [hour, setHour] = useState(Math.floor(userGoal / 60))
+  const [minute, setMinute] = useState(userGoal % 60)
 
-
+  useEffect(() => {
+    setMyGoal(
+      {
+        ...myGoal,
+        [category]: {
+          ...myGoal[category],
+          userGoal: 60 * hour + minute
+        }
+      }
+    )
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [hour, minute])
   return (
     <TableRow className='text-2xl text-center whitespace-nowrap text-white bg-[#92C7CF88] border-none hover:bg-[#92C7CF88]'>
       <TableCell className={`font-bold py-6 text-center ${(isLastRow) && 'rounded-bl-2xl'} `}>{category}</TableCell>
       <TableCell className={`${isEdit && 'px-0 pb-0 pt-3 flex items-center h-full whitespace-nowrap'}`}>
         {isEdit ?
           <>
-            <Input type="number" value={userGoal} className="border-none shadow-none text-2xl bg-white text-black h-full"
-              onChange={(e) => {
-                setMyGoal(
-                  {
-                    ...myGoal,
-                    [category]:{
-                      ...myGoal[category],
-                      userGoal: parseInt(e.target.value)
-                    }
-                  }
-                )
-              }}
+            <Input type="number" value={hour} className="border-none shadow-none text-2xl bg-white text-black h-full"
+              onChange={(e) => setHour(parseInt(e.target.value))}
             />
-            <div className="ml-3 h-full">분</div>
+            <div className="ml-1 h-full">시</div>
+            <Input type="number" value={minute} className="border-none shadow-none text-2xl bg-white text-black h-full ml-3"
+              onChange={(e) => setMinute(parseInt(e.target.value))}
+            />
+            <div className="ml-1 h-full">분</div>
           </>
           : category === '기상' ? `${Math.floor(userGoal / 60).toString()}시 ${userGoal % 60}분` : `${userGoal}분`
         }
@@ -47,4 +54,4 @@ const MyGoalRow: React.FC<{
   )
 }
 
-export default MyGoalRow
+export default GetUpRow
