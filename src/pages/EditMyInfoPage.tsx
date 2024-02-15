8226f78bdb0bcefb7db0cd534b39985b177a6a27
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2024-02-15 16:39:29 +0900

    Feat: fetch profile

--- src/pages/EditMyInfoPage.tsx
+++ src/pages/EditMyInfoPage.tsx
@@ -1,17 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import MyForm from '../components/EditMyInfoPage/MyForm'
 import PasswordChangeForm from '../components/EditMyInfoPage/PasswordChangeForm'
 import ProfileImageUpload from '../components/EditMyInfoPage/ProfileImageUpload'
 import Modal from '../components/EditMyInfoPage/Modal'
+import { fetchProfile } from "../api/profile/fetchProfile"
+import { UserInfo } from "../types/UserInfo.type"
+
+const initialUserInfo: UserInfo = {
+  userName: "",
+  nickname: "",
+  email: "",
+  createdAt: new Date().toDateString(),
+  contactNumber: "",
+  introduction: "",
+  imageUrl: "",
+  socialType: "HARU"
+}
 
 const EditMyInfoPage = () => {
+
+  const [userInfo, setUserInfo] = useState<UserInfo>(initialUserInfo);
+
+  useEffect(() => {
+    const getUserInfo = async () => {
+      const response = await fetchProfile();
+      setUserInfo(response.data);
+    }
+    getUserInfo();
+  }, [])
+
   return (
     <div className="bg-gray-100 flex justify-center items-center font-ibm">
       <div className="bg-white rounded-lg w-full">
         {/* Top Section */}
         <div className="flex ">
           <div className="w-1/4 p-3 mt-6 ">
-            <ProfileImageUpload />
+            <ProfileImageUpload userName={userInfo.userName} email={userInfo.email} createdAt={userInfo.createdAt} imageUrl={userInfo.imageUrl} />
           </div>
           <div className="w-3/4 p-4">
             <MyForm />
