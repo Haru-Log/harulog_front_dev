40040f299fb6ed0b1f046b8b065b9e3c3b500066
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2024-02-17 21:52:40 +0900

    Fix: feeditem, commenttype api version

--- src/types/FeedItem.type.ts
+++ src/types/FeedItem.type.ts
@@ -1,57 +1,18 @@
-//dummyImages
-import dummyImage1 from '../assets/20231010_084411.jpg'
-import dummyImage2 from '../assets/20231121_091209.jpg'
-import dummyImage3 from '../assets/achievement-5597527_640.png'
-import dummyImage4 from '../assets/kakao_icon.png'
-import dummyImage5 from '../assets/20231029_091459.png'
-import dummyImage6 from '../assets/Screenshot_20200427-152644_Facebook.png'
-import dummyImage7 from '../assets/Screenshot_20220714-051432_Instagram.png'
-import dummyImage8 from '../assets/FB_IMG_1695830410274.jpg'
-import dummyImage9 from '../assets/FB_IMG_1613092264309.jpg'
-import dummyImage10 from '../assets/20231020_090539.jpg'
+import { CommentType } from "./CommentType";
 
 export interface FeedItem {
-  post_id: number;
-  user_idx: number;
+  id: number;
+  nickname: string;
   category_name: string;
   content: string;
-  post_image: string;
+  imgUrl: string;
   like: number;
   comment: number;
-  created_at: Date;
-  updated_at?: Date;
-  achievement: number;
+  createdAt: Date;
+  updateAt?: Date;
+  activityTime: number;
+  goal: number;
+  commentList?: CommentType[]
+  likeCount: number;
+  commentCount: 6;
 }
-
-export const dummy_sample: FeedItem[] = [
-  {
-    content: '하체 털었는 날', post_image: dummyImage1, like: 3, comment: 5, category_name: '운동', user_idx: 1, post_id: 1, achievement: 60, created_at: new Date(1704750206013)
-  },
-  {
-    content: '가슴 맛있게 먹은 날', post_image: dummyImage2, like: 3, comment: 5, category_name: '운동', user_idx: 1, post_id: 2, achievement: 40, created_at: new Date(1704750206013 - 3)
-  },
-  {
-    content: '미라클모닝', post_image: dummyImage3, like: 3, comment: 5, category_name: '기상', user_idx: 1, post_id: 3, achievement: 390, created_at: new Date(1704750206013 - 6)
-  },
-  {
-    content: '겨우 일어난 날', post_image: dummyImage4, like: 3, comment: 5, category_name: '기상', user_idx: 1, post_id: 4, achievement: 450, created_at: new Date(1704750206013 + 500)
-  },
-  {
-    content: '공부하기싫다', post_image: dummyImage5, like: 3, comment: 5, category_name: '공부', user_idx: 1, post_id: 5, achievement: 60, created_at: new Date(1704750206013 - 12)
-  },
-  {
-    content: '공부하기싫다22', post_image: dummyImage6, like: 3, comment: 5, category_name: '공부', user_idx: 1, post_id: 6, achievement: 80, created_at: new Date(1704740206013 - 15)
-  },
-  {
-    content: '인상 깊었던 구절', post_image: dummyImage7, like: 3, comment: 5, category_name: '독서', user_idx: 1, post_id: 7, achievement: 20, created_at: new Date(1704740206013 - 15)
-  },
-  {
-    content: '인상 깊었던 구절', post_image: dummyImage8, like: 3, comment: 5, category_name: '독서', user_idx: 1, post_id: 8, achievement: 40, created_at: new Date(1704740206013 - 15)
-  },
-  {
-    content: '인상 깊었던 구절', post_image: dummyImage9, like: 3, comment: 5, category_name: '독서', user_idx: 1, achievement: 30, post_id: 9, created_at: new Date(1704740206013 - 15)
-  },
-  {
-    content: '등 맛있게 먹은 날', post_image: dummyImage10, like: 3, comment: 5, category_name: '운동', user_idx: 1, achievement: 60, post_id: 10, created_at: new Date(1704740206013 - 15)
-  },
-]
