ee157414be514dc1b13489322a0e279e15de0b38
childevnote <daniel011125@gmail.com>
2024-02-20 12:41:49 +0900

    Chore: chatlist type added in chatList store

--- src/zustand/chatStore.ts
+++ src/zustand/chatStore.ts
@@ -1,7 +1,10 @@
 import create from 'zustand';
 import { devtools } from 'zustand/middleware';
+import { ChatList } from '../types/ChatList.type';
 
 interface ChatState {
+  chatList: ChatList[];
+  setChatList: (chatList: ChatList[]) => void;
   selectedChatroomId: number;
   selectChatroom: (chatroomId: number) => void;
 }
@@ -9,6 +12,8 @@ interface ChatState {
 export const useChatStore = create<ChatState>()(
   devtools(
     (set) => ({
+      chatList: [],
+      setChatList: (chatList: ChatList[]) => set({ chatList }),
       selectedChatroomId: 0,
       selectChatroom: (selectedChatroomId: number) => set({ selectedChatroomId }),
     }),
