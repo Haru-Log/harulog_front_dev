0b80f70807ad3c2c4e56c46f5bce09fb53b20527
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2024-02-22 02:33:29 +0900

    Fix: social page profile img

---
+++ src/components/SocialPage/SocialProfileImg.tsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react'
+import { fetchImgFromFirebase } from './../../api/fetchImgFirebase';
+
+const SocialProfileImg: React.FC<{ imageUrl: string; index: number }> = ({ imageUrl, index }) => {
+
+  const [profileImg, setProfileImg] = useState("");
+  useEffect(() => {
+    fetchImgFromFirebase(imageUrl).then((res) => setProfileImg(res))
+  }, [imageUrl])
+
+  return (
+    <img src={profileImg} alt={`profile-img-${index}`} className='rounded-full w-12'></img>
+  )
+}
+
+export default SocialProfileImg
